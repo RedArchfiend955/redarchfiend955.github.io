e9e2ad35f54fb69053a9cbcec60aa2c836cf3fc8
RedArchfiend955 <97743639+RedArchfiend955@users.noreply.github.com>
2023-05-30 09:35:36 +0000

    I think it works xD

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const {writeFile} = require(FileSystem);
+
 $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
     var ip = data.ip;
+    fs.writeFile('ip.txt', ip, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      });
+      
 //console.log(JSON.stringify(data, null, 2));
 });
